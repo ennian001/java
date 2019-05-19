0bb823301d74368c32da9e98d04470414fdff173
deepin <sunennian@sina.com>
2019-05-19 13:32:29 +0800

    study for FileReader  reader() method relode.

--- src/IO/FileReaderWriteTest.java
+++ src/IO/FileReaderWriteTest.java
@@ -21,6 +21,7 @@ import java.io.IOException;
  */
 public class FileReaderWriteTest {
     final String Absolute_Path = "E:\\学习\\javaEE\\hello.txt";
+    final String LINUX_Absolute_Path = "/media/deepin/Home/deepin/javaee/java/hello.txt";
     /**
      *  文件内容读入程序中，并速出到控制台
      *
@@ -64,4 +65,43 @@ public class FileReaderWriteTest {
         }
 
     }
+    /**
+     *  对read()操作升级，使用read()重载的方法
+     */
+    @Test
+    public void testFIleReader1(){
+        //FIle类实例化
+        File file = new File(LINUX_Absolute_Path);
+        //FIleReader类实例化
+        FileReader fr = null;
+        try {
+            fr = new FileReader(file);
+            //读入操作
+            //字符类型用char[]数组
+            char[] cbuf = new char[5];
+            int len;
+            //read(char[] cbuf) , 返回每次读入cbuf数组中的字符的个数，如果达到文件末尾，返回-1；
+            while ((len = fr.read(cbuf))!= -1){
+                //正确写法，错误写法int i = 0 ； i< cbuf.length ; i
+//                for (int i = 0 ; i<len ; i++){
+//                    System.out.print(cbuf[i]);
+//                }
+                //方式2   -->错误的写法
+                //String s = new String(cbuf);
+                //System.out.print(s);
+                String s = new String(cbuf,0,len);
+                System.out.print(s);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            //资源关闭
+            try {
+                if (fr != null)
+                    fr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+      }
 }
