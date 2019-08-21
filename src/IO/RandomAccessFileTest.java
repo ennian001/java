c396845738857aeca84fcf086c2575149ce500dc
sunennian <sunennian@sina.com>
2019-08-21 22:49:56 +0800

    随机访问流 学习

--- src/IO/RandomAccessFileTest.java
+++ src/IO/RandomAccessFileTest.java
@@ -1,11 +1,9 @@
 package IO;
 
+
 import org.junit.Test;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.io.*;
 
 /**
  *  RandomAccessFile的使用
@@ -52,6 +50,41 @@ public class RandomAccessFileTest {
         }
 
 
+    }
+    //实现文件的替换
+    @Test
+    public void test2() throws IOException {
+       RandomAccessFile raf1 = new RandomAccessFile("hello.txt","rw");
+
+       raf1.seek(3);//将指针调到角标为3的位置
+       raf1.write("xyz".getBytes());
+       raf1.close();
+    }
+    /**
+     *  使用RandomAccessFile实现数据的插入操作
+     */
+    @Test
+    public void test3() throws IOException {
+       RandomAccessFile raf1 = new RandomAccessFile("hello.txt","rw");
+        raf1.seek(3);//将指针调到角标为3的位置
+        //保存指针3后面的所有数据到StringBuilder中
+        //StringBuilder stringBuilder = new StringBuilder((int) new File("hello.txt").length());
+        //方式二、改造成为ByteArrayOutputStream
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        byte[] buffer = new byte[20];
+        int len ;
+        while ((len = raf1.read(buffer))!=-1){
+            //stringBuilder.append(new String(buffer,0,len));
+            //写到ByteArrayOutputStream     字节数组中
+            baos.write(buffer,0,len);
+        }
+        //调会指针，写入“xyz”
+        raf1.seek(3);
+        raf1.write("xyz".getBytes());
+        //将StringBuilder中的数据写入到文件中
+        //raf1.write(stringBuilder.toString().getBytes());
+        raf1.write(baos.toByteArray());
+        raf1.close();
     }
 
 
