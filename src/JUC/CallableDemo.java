a3974677fc8e2e9a5c6473389cb3a469126ad40c
sunennian <sunennian@sina.com>
2019-08-22 21:22:29 +0800

    Callable接口学习 学习

---
+++ src/JUC/CallableDemo.java
@@ -0,0 +1,39 @@
+package JUC;
+
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+class MyThreadCallInstinst implements Callable<Integer>{
+
+    /**
+     * Computes a result, or throws an exception if unable to do so.
+     *
+     * @return computed result
+     * @throws Exception if unable to compute a result
+     */
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("***************************call****************");
+        return 200;
+    }
+}
+
+public class CallableDemo {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        /*
+        *  FutureTask 传入Callable实现类
+        *  虽然开始两次线程，但是查看资源仅仅调用了一次
+        * */
+        FutureTask<Integer> ft = new FutureTask<Integer>(new MyThreadCallInstinst());
+        new Thread(ft,"AA").start();
+        new Thread(ft,"BB").start();
+        //获取返回参数
+        Integer integerA = ft.get();
+        System.out.println(integerA);
+        Integer integerB = ft.get();
+        System.out.println(integerB);
+    }
+
+}
